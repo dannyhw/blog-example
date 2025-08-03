311f9658e70c88ad3382d04fae49a3c5096ccb53
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-08-03 23:01:27 +0100

    simpler config

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,4 @@
-import { type ConfigContext, type ExpoConfig } from "expo/config";
+import { type ExpoConfig } from "expo/config";
 
 const BUNDLE_MAPPING = {
   development: "com.dannyhw.blogexample-development",
@@ -15,63 +15,63 @@ if (!bundleId) {
   throw new Error(`EXPO_PUBLIC_ENVIRONMENT is not set correctly`);
 }
 
-export default ({ config }: ConfigContext): ExpoConfig => {
-  return {
-    name: "blog-example",
-    slug: "blog-example",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/images/icon.png",
-    scheme: "blogexample",
-    userInterfaceStyle: "automatic",
-    newArchEnabled: true,
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: bundleId,
-      infoPlist: {
-        ITSAppUsesNonExemptEncryption: false,
-      },
-      appleTeamId: "KUFRY2HT2D",
+const config: ExpoConfig = {
+  name: "blog-example",
+  slug: "blog-example",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "blogexample",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: bundleId,
+    infoPlist: {
+      ITSAppUsesNonExemptEncryption: false,
     },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/images/adaptive-icon.png",
+    appleTeamId: "KUFRY2HT2D",
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+    edgeToEdgeEnabled: true,
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
         backgroundColor: "#ffffff",
       },
-      edgeToEdgeEnabled: true,
-    },
-    web: {
-      bundler: "metro",
-      output: "static",
-      favicon: "./assets/images/favicon.png",
-    },
-    plugins: [
-      "expo-router",
-      [
-        "expo-splash-screen",
-        {
-          image: "./assets/images/splash-icon.png",
-          imageWidth: 200,
-          resizeMode: "contain",
-          backgroundColor: "#ffffff",
-        },
-      ],
     ],
-    experiments: {
-      typedRoutes: true,
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+  extra: {
+    router: {},
+    eas: {
+      projectId: "480eeae0-ebf4-4a54-8afe-cc1066b3ffd7",
     },
-    extra: {
-      router: {},
-      eas: {
-        projectId: "480eeae0-ebf4-4a54-8afe-cc1066b3ffd7",
-      },
-    },
-    owner: "dannyhw",
-    runtimeVersion: {
-      policy: "appVersion",
-    },
-    updates: {
-      url: "https://u.expo.dev/480eeae0-ebf4-4a54-8afe-cc1066b3ffd7",
-    },
-  };
+  },
+  owner: "dannyhw",
+  runtimeVersion: {
+    policy: "appVersion",
+  },
+  updates: {
+    url: "https://u.expo.dev/480eeae0-ebf4-4a54-8afe-cc1066b3ffd7",
+  },
 };
+
+export default config;
